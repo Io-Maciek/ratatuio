ab40101384950e0de77a330e71fd43025c33d343
Maciej <mkuchcik@gmail.com>
2024-12-13 15:52:04 +0100

    0.0.7 publish

--- src/view.rs
+++ src/view.rs
@@ -5,7 +5,7 @@ use ratatui::{buffer::Buffer, layout::Rect, widgets::WidgetRef};
 use std::io;
 
 /// Trait representing a view of application.
-/// 
+///
 /// This trait can be used to define the rendering [`View::render_view()`] of its properties and how it should handle events [`View::handle_events()`].
 pub trait View {
     fn handle_events(&mut self, _event: &Event) -> io::Result<()> {
@@ -17,8 +17,8 @@ pub trait View {
 
 pub(crate) struct ViewWidgetWrapper<'a>(pub(crate) &'a Box<dyn View + Send + Sync>);
 
-impl <'a> WidgetRef for ViewWidgetWrapper <'a>{
-    fn render_ref(&self, area: Rect, buf: &mut Buffer){
+impl<'a> WidgetRef for ViewWidgetWrapper<'a> {
+    fn render_ref(&self, area: Rect, buf: &mut Buffer) {
         self.0.render_view(area, buf);
     }
 }
